d1661a1cdf32928f63988a6a3d83ce5808dbf11a
Misko <m.hresko.dev@gmail.com>
2026-01-16 00:42:29 +0100

    learning more things about logical operators

---
+++ logical-operators.js
@@ -0,0 +1,5 @@
+let userColor = 'red';
+let defaultColor = 'blue';
+let currentColor = userColor || defaultColor;
+
+console.log(currentColor)
---
+++ ternary-operator.js
@@ -0,0 +1,4 @@
+let points = 100;
+
+let result = points > 100 ? 'gold' : 'silver';
+console.log(result);
