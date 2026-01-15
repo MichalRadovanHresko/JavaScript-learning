66002da8e174f27ede1e62fceecb9da5b1127317
Misko <m.hresko.dev@gmail.com>
2026-01-15 23:39:56 +0100

    increment and decrement operators new information

---
+++ new.js
@@ -0,0 +1,16 @@
+const y = 2;
+let x = 3;
+
+// console.log(y**x);
+
+// increment values 
+
+console.log(x++);
+// difference between ++x and x++ ! 
+console.log(x);
+
+// decrement values 
+
+console.log(--x);
+console.log(x--);
+console.log(x);
