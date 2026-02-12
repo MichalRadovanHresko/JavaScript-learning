a935eb92a1ae4955f07c91c35248d5c5717be6dc
Misko <m.hresko.dev@gmail.com>
2026-02-12 23:54:46 +0100

    CodeWars Exercise

---
+++ Exercises/CodeWars/isUndefined.js
@@ -0,0 +1,6 @@
+// The function is supposed to check if the input is undefined, 
+// but it is not working correctly. Can you fix the code ?
+
+function isUndefined(value) {
+    return value === undefined;
+}
