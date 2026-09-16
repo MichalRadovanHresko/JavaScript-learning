8703b91365e753e9d27b0648954bdd9890b2eb43
Misko <m.hresko.dev@gmail.com>
2026-09-16 19:48:15 +0200

    Leetcode exercise - validParantheses

---
+++ Exercises/LeetCode/validParantheses.js
@@ -0,0 +1,22 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+// determine if the input string is valid.
+// An input string is valid if:
+//     Open brackets must be closed by the same type of brackets.
+//     Open brackets must be closed in the correct order.
+//     Every close bracket has a corresponding open bracket of the same type.
+
+const isValid = (s) => {
+  const pairs = { ")": "(", "}": "{", "]": "[" };
+  const stack = [];
+
+  for (const char of s) {
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else {
+      if (stack.pop() !== pairs[char]) return false;
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid("(([]){})"));
